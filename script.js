495cca03a256cbef23ecb795bbe62ee924f1f405
szymon-f <sfluder25@gmail.com>
2022-08-05 15:40:12 +0200

    :complete html and css

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 
 function add(a, b){
+    a = +a;
+    b = +b;
     return a + b;
 }
 
 function subtract(a, b){
+    a = +a;
+    b = +b;
     return a - b;
 }
 
 function multiply(a, b){
+    a = +a;
+    b = +b;
     return a * b;
 }
 
 function divide(a, b){
+    a = +a;
+    b = +b;
     if (b === 0){
         return "You can't divide by zero 😡"
     }
     return a / b;
 }
 
-console.log(multiply(15, 7))
+
+function operate(operator, a, b){
+    switch(operator){
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "An error occured"
+    }
+}
+
