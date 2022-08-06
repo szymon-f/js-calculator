9bbbdbeac68daf056053f4c8650d76f10ce0f17c
szymon-f <sfluder25@gmail.com>
2022-08-06 09:58:06 +0200

    complete functionality

--- script.js
+++ script.js
@@ -21,13 +21,17 @@ function divide(a, b){
     a = +a;
     b = +b;
     if (b === 0){
-        return "You can't divide by zero 😡"
+        alert("You can't divide by zero 😡");
+        const display = document.querySelector('.calculator-screen');
+        document.textContent = "";
+        displayValue = "";
+        return "";
     }
     return a / b;
 }
 
 
-function operate(operator, a, b){
+function operate(a, b, operator){
     switch(operator){
         case "+":
             return add(a, b);
@@ -38,7 +42,8 @@ function operate(operator, a, b){
         case "/":
             return divide(a, b);
         default:
-            return "An error occured"
+            alert("don't input = before an expression to evaluate!")
+            return ""
     }
 }
 
@@ -51,15 +56,59 @@ function refreshDisplay(){
     display.textContent += lastInputValue;
 }
 
-function addBtnFunctionality(){
+function addNumBtnFunctionality(){
     const btns = document.querySelectorAll('li');
     btns.forEach(button => {
         button.addEventListener('click', () => {
-            displayValue += button.id;
-            lastInputValue = button.id;
+            displayValue += button.textContent;
+            lastInputValue = button.textContent;
             refreshDisplay();
         })
     })
 }
 
-addBtnFunctionality();
+function addClearBtnFunctionality(){
+    const btn = document.querySelector('#clear');
+    btn.addEventListener('click', () => {
+        displayValue = "";
+        lastInputValue = "";
+        clearDisplay();
+    })
+}
+
+function clearDisplay(){
+    const display = document.querySelector('.calculator-screen');
+    display.textContent = "";
+}
+
+function addEqualBtnFunctionality(){
+    const btn = document.querySelector("#equal");
+    btn.addEventListener('click', () => {
+        let [a, b, operator] = parseInput(displayValue);
+        let result = operate(a, b, operator);
+        // console.log(result);
+        const display = document.querySelector('.calculator-screen');
+        display.textContent = result;
+        displayValue = result;
+    })
+}
+
+function parseInput(str){
+    const operators = ['+', '-', '/', '*'];
+    let left = "";
+    let right = ""
+    let operator = "";
+    for(let i = 0; i < str.length; i++){ // TO MOŻNA ZROBIĆ str.split()!!
+        if(operators.includes(str[i])){
+            left = str.slice(0, i);
+            right = str.slice(i + 1, -1);
+            operator = str[i];
+        }
+    }
+    // console.log([left, right, operator]);
+    return [left, right, operator];
+}
+
+addNumBtnFunctionality();
+addClearBtnFunctionality();
+addEqualBtnFunctionality();
