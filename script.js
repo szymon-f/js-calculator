cc9584d148e3c1a5294c8615f4f9aa70f5b8a5e5
szymon-f <sfluder25@gmail.com>
2022-08-07 11:29:04 +0200

    add support for multiple operations in one go

--- script.js
+++ script.js
@@ -53,10 +53,13 @@ const numBtns = document.querySelectorAll('.number');
 const equalBtn = document.querySelector("#equal");
 const clearBtn = document.querySelector('#clear');
 const operatorBtns = document.querySelectorAll('.operator');
+const previousOperationP = document.querySelector('.previous-operation');
+const currentOperationP = document.querySelector('.current-operation');
 
 let leftOperand = "";
 let rightOperand = "";
 let operator = "";
+let previousOperation = "";
 
 function refreshDisplay(result){
     if(arguments.length === 0){
@@ -86,6 +89,12 @@ function addNumBtnFunctionality(){
 function addOperatorBtnFunctionality(){
     operatorBtns.forEach(button => {
         button.addEventListener('click', () => {
+            if(leftOperand.length !== 0 && operator.length !== 0  && rightOperand.length !== 0){
+                let result = operate(leftOperand, rightOperand, operator);
+                refreshDisplay(result);
+                leftOperand = result;
+                rightOperand = "";
+            }
             operator = button.textContent;
             refreshDisplay();
         })
